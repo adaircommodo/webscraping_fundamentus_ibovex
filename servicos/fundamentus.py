d61055dacf8f8980016fd7452dabdf1d76c84745
Adair J. Rossatto Commodo <adaircommodo@gmail.com>
2022-09-11 09:38:23 -0300

    Saida de dados para um Dataframe pandas

--- servicos/fundamentus.py
+++ servicos/fundamentus.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import csv
 import requests as rq
 from bs4 import BeautifulSoup
 #---
@@ -14,6 +16,7 @@ class Fundamentus:
                 "X-Requested-With": "XMLHttpRequest"
             }
         
+
     def printContentScreen(self, addIbvx = 0, **kwargs):
         for fii in self.fiis:
             #site FUNDAMENTUS 
@@ -46,3 +49,34 @@ class Fundamentus:
                 except KeyError:
                     pregoes.printContentScreen()
 
+
+    def outContentCsv(self, addIbvx = 0, **kwargs):
+        dados = []
+        ttFundos = len(self.fiis)
+        ctFundo = 1
+        for fii in self.fiis:
+            #site FUNDAMENTUS 
+            target_url = 'https://www.fundamentus.com.br/detalhes.php?papel='+fii
+            target_page = rq.get( target_url, headers=self.header )
+
+            if(target_page.status_code!=200):
+                print(f'Ocorreu um erro na requisição da página: {target_page}')
+                break
+
+            page = BeautifulSoup(target_page.text, 'html.parser')
+            tables = page.find_all('table')
+            cotacao = conv.strToFloat( conv.comaToPoint( tables[0].find_all('td', attrs={'class':'data destaque w3'})[0].find('span').text ) )
+            segmento = tables[0].find_all('tr')[3].find_all('td', attrs={'class':'data'})[0].find('span').text
+            ffoyield = conv.strToFloat( conv.comaToPoint( tables[2].find_all('tr')[1].find_all('td', attrs={'class':'data'})[1].find('span').text ) )
+            divyield = conv.strToFloat( conv.comaToPoint( tables[2].find_all('tr')[2].find_all('td', attrs={'class':'data'})[1].find('span').text ) )
+            ppv = conv.strToFloat( conv.comaToPoint( tables[2].find_all('tr')[3].find_all('td', attrs={'class':'data'})[1].find('span').text ) )
+            vpcota = conv.strToFloat( conv.comaToPoint( tables[2].find_all('tr')[3].find_all('td', attrs={'class':'data'})[2].find('span').text ) )
+
+            print(f'Lendo em fundamentus - ({ctFundo} de {ttFundos}) FUNDO: {fii}...\n')
+            dados.append({'fundo':fii,'segmento':segmento, 'cotacao':cotacao, 'ffoyield':ffoyield, 'divyield':divyield, 'ppv':ppv, 'vpcota':vpcota})
+            ctFundo = ctFundo+1
+        
+        df = pd.DataFrame(dados)
+        print(df)
+        #df_out = pd.to_csv(dados)
+            
